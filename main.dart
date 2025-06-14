e6bd5af1254cff3b48db3ee51d196e9f672795bd
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-06-14 16:54:32 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
-import 'views/login/login_page.dart';
+import 'package:tism/views/login/login_page.dart';
+import 'package:tism/views/home/home_page.dart'; // Importe sua HomePage
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // <-- Corrigido o nome da classe
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: const LoginPage(), // <-- Agora começa na tela de login
+      title: 'TISM App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginPage(),
+        // CORREÇÃO AQUI: Passe um valor para 'nomeUsuario'
+        '/home': (context) => const HomePage(nomeUsuario: 'Usuário Logado'),
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
