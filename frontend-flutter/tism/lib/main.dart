b9753c767c97ad66498f4cb22d6d6fc32780ff57
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-06-24 20:22:38 -0300

    main

--- frontend-flutter/tism/lib/main.dart
+++ frontend-flutter/tism/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tism/views/login/login_page.dart';
 import 'package:tism/views/home/home_page.dart'; // Importe sua HomePage
+
 void main() {
   runApp(const MyApp());
 }
