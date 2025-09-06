9f875951b2dcdee9a95faa7bbc009ea647b9067a
PedroLabastie <pedrodesouzalabastie@gmail.com>
2025-09-06 16:40:32 -0300

    Fix UserPostsController naming and model references

--- backend-rails/app/controllers/user_posts_controller.rb
+++ backend-rails/app/controllers/user_posts_controller.rb
@@ -1,12 +1,12 @@
 class UserPostsController < ApplicationController
   def show
-    post = Post.find(params[:id])
+    post = UserPost.find(params[:id])
     comments = post.comments.includes(:user)
     render json: { post: post, comments: comments.as_json(include: :user) }
   end
 
   def create
-    post = Post.new(params.permit(:title, :content, :forum_id, :user_id))
+    post = UserPost.new(params.permit(:title, :content, :forum_id, :user_id))
     if post.save
       render json: post, status: :created
     else
