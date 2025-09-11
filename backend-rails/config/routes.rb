838c887ecdad0bc41bd17efdf41b102260775670
PedroLabastie <pedrodesouzalabastie@gmail.com>
2025-09-10 22:06:29 -0300

    🔧 FIX: Rota raiz para health check - resolve erro 404 no deploy

--- backend-rails/config/routes.rb
+++ backend-rails/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "rails/health#show"
   post "/signup", to: "users#create"
   post "/login", to: "users#login"
   put "/users/:id", to: "users#update"
