e05acdc5872094f94825cc208fe28c48b1d17b34
PedroLabastie <pedrodesouzalabastie@gmail.com>
2025-09-18 14:39:43 -0300

    🔧 Fix: Logger configuration error causing Failed to fetch

--- backend-rails/config/environments/production.rb
+++ backend-rails/config/environments/production.rb
@@ -76,9 +76,8 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-    logger.formatter = config.log_formatter
-    config.logger    = logger
+    logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+    config.logger = logger
   end
 
   # Do not dump schema after migrations.
